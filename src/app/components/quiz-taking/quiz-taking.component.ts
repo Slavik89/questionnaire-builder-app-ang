5cc4b2e708de95790f24918ade9eac7e27c92a2b
Slavik89 <terakrokos@gmail.com>
2025-03-25 11:02:19 +0200

    [Added] Timer was added to the quiz-taking component

--- src/app/components/quiz-taking/quiz-taking.component.ts
+++ src/app/components/quiz-taking/quiz-taking.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuizDataService } from '../../services/quiz-data/quiz-data.service'; 
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
@@ -9,17 +9,24 @@ import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
   templateUrl: './quiz-taking.component.html',
   styleUrl: './quiz-taking.component.scss'
 })
-export class QuizTakingComponent implements OnInit {
+export class QuizTakingComponent implements OnInit, OnDestroy {
 
   quizTitle: string | undefined = "";
   quizQuestions: any[] | undefined = [];
 
   quizTakingForm: FormGroup;
 
+  timer: number = 0;  
+  elapsedTime: string = "00:00:00";
+
+  private startTime!: number;
+  private timerInterval!: any;
+
   constructor(private router: Router, private quizDataService: QuizDataService, private fb: FormBuilder) {
     // Ініціалізація форми
     this.quizTakingForm = this.fb.group({
       questions: this.fb.array([]),
+      totalTime: [0]
     });
   }
 
@@ -36,7 +43,11 @@ export class QuizTakingComponent implements OnInit {
     console.log('quiz-taking', this.quizTitle, this.quizQuestions);
 
     this.loadQuestions();
+    this.startTimer();
+  }
 
+  ngOnDestroy(): void {
+    this.stopTimer(); 
   }
 
   loadQuestions() {
@@ -78,10 +89,43 @@ export class QuizTakingComponent implements OnInit {
   get questions(): FormArray {
     return this.quizTakingForm.get('questions') as FormArray;
   }
-  
+
+    // Запуск таймера
+  startTimer() {
+    this.startTime = Date.now();
+    this.timerInterval = setInterval(() => {
+      const elapsedMs = Date.now() - this.startTime;
+      const totalSeconds = Math.floor(elapsedMs / 1000);
+      const hours = Math.floor(totalSeconds / 3600);
+      const minutes = Math.floor((totalSeconds % 3600) / 60);
+      const seconds = totalSeconds % 60;
+
+      this.elapsedTime = `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
+    }, 1000);
+  }
+
+  pad(value: number): string {
+    return value.toString().padStart(2, "0");
+  }
+
+    // Зупинка таймера
+  stopTimer() {
+    if (this.timerInterval) {      
+      clearInterval(this.timerInterval);
+    }
+  }  
 
   onSubmit() {
     if (this.quizTakingForm.valid) {
+
+
+      this.stopTimer();
+      // Запис фінального часу у totalTime
+      const elapsedMs = Date.now() - this.startTime;
+      const totalSeconds = Math.floor(elapsedMs / 1000);
+      this.quizTakingForm.patchValue({ totalTime: totalSeconds });
+
+      console.log('Total time spent:', this.elapsedTime);
       console.log('Form Value:', this.quizTakingForm.value);
     }
   }
